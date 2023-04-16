652eef6bee3cf1f82c464385112ae06e153003fe
edySnake <contactoedysnake@gmail.com>
2023-04-16 04:15:42 -0600

    Radio e iconos

---
+++ KyclopsRadioWepApp/wwwroot/js/RadioJs.js
@@ -0,0 +1,18 @@
+﻿    var audio = document.getElementById("radio");
+    var isPlaying = false;
+
+    function togglePlay() {
+            if (isPlaying) {
+        audio.pause();
+    document.getElementById("play-button").classList.add("fa-play-circle");
+    document.getElementById("play-button").classList.remove("fa-pause-circle");
+            } else {
+        audio.play();
+    document.getElementById("play-button").classList.add("fa-pause-circle");
+    document.getElementById("play-button").classList.remove("fa-play-circle");
+            }
+    isPlaying = !isPlaying;
+        }
+
+    document.getElementById("play-button").addEventListener("click", togglePlay);
+
