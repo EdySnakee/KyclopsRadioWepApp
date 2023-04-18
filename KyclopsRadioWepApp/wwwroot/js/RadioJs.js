497aace15dd87953a7c1da49d40272300e774068
edySnake <contactoedysnake@gmail.com>
2023-04-18 16:40:54 -0600

    Play arreglado

--- KyclopsRadioWepApp/wwwroot/js/RadioJs.js
+++ KyclopsRadioWepApp/wwwroot/js/RadioJs.js
@@ -16,3 +16,24 @@
 
     document.getElementById("play-button").addEventListener("click", togglePlay);
 
+
+
+
+var audio = document.getElementById("radio");
+var isPlaying = false;
+
+function togglePlay() {
+    if (isPlaying) {
+        audio.pause();
+        document.getElementById("play-button2").classList.add("fa-play-circle");
+        document.getElementById("play-button2").classList.remove("fa-pause-circle");
+    } else {
+        audio.play();
+        document.getElementById("play-button2").classList.add("fa-pause-circle");
+        document.getElementById("play-button2").classList.remove("fa-play-circle");
+    }
+    isPlaying = !isPlaying;
+}
+
+document.getElementById("play-button2").addEventListener("click", togglePlay);
+
